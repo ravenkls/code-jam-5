e5dea71f473c90e95bbcf2e078cdb44725f8d2f6
zebralt <jocelyn.vernay@gmail.com>
2019-07-02 22:22:43 +0200

    Update theworldcount & tts in /ads

--- only_otters/ads/facts/theworldcounts/theworldcounts.py
+++ only_otters/ads/facts/theworldcounts/theworldcounts.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
 
     hxq = HierarchicalXPathQuery.from_yml(__folder__ / 'theworldcounts.yml')
 
-    pulled_resources = [ *hxq() ]
+    pulled_resources = list(*hxq())
 
     print(json.dumps(
         pulled_resources,
